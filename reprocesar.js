4cfc9b17332dc04b18ce5137b2cc361548177b2e
Agustt123 <agustintracheskyoficial@gmail.com>
2025-10-06 14:22:06 -0300

    dsadasdas

--- reprocesar.js
+++ reprocesar.js
@@ -34,7 +34,7 @@ async function enviarMensajes() {
 
         console.log("Consultando base de datos...");
         const [rows] = await con.query(
-            `SELECT resource, seller_id, autofecha FROM db_shipments WHERE autofecha >= ? AND autofecha <= ? and seller_id = 2017830246 `,
+            `SELECT resource, seller_id, autofecha FROM db_shipments WHERE autofecha >= ? AND autofecha <= ? and seller_id = 135378723 `,
             [FECHA_INICIO, FECHA_FIN]
         );
         console.log(`🔍 Se encontraron ${rows.length} registros para enviar.`);
