9f13fdb436c9a6fe0066dc1e46b09d4b6d8b848d
Agustt123 <agustintracheskyoficial@gmail.com>
2025-12-18 11:45:54 -0300

    dsadasd

--- reprocesar.js
+++ reprocesar.js
@@ -17,7 +17,7 @@ const con = mysql.createPool({
 });
 
 // Rangos de fecha (modificables)
-const FECHA_INICIO = "2025-16-11 00:30:00";
+const FECHA_INICIO = "2025-12-16 00:30:00";
 const FECHA_FIN = "2025-12-19 23:50:00";
 
 // Función principal
